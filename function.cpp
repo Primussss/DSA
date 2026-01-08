5e0d6de8aa5cd15f52d2485117734696652e7977
Abhishek Ghadmode <abhighadmode1@gmail>
2026-01-08 13:56:20 +0530

    functions.cpp

---
+++ function.cpp
@@ -0,0 +1,35 @@
+/* people of this time does not understand the importance of time thry are wasting on screes specially those six inches 
+bastards are the ones truely to the balme for the next generation kids being so dumb or so smart but yet none of them wants to read
+a book now nor they like the exceptional cartoons and animes whoch airs on television now each of them have their own smartphone and 
+they can search for alomst anyrhing they want no matter ehat kind of stuuf it is weather be porn or some of the most disturbing things
+that could be found on the internet nowadays.i have thse concrns for those new age kida beacuse of my personal hooros that i experienced
+with that phone thing when i was in my growth ages, there is this one video i truely wish i would had naver seen in y entire life and
+yet that curios brain and eyes of that small kid were to see that stuff. it wad not truely a ghost or anyhting just some humans killing 
+other humans but on a mass scale they did it with the help of a wire , a fast moving wire that sliced each of the members on that boat so 
+so neatly that some of them didn't even knew they were killed theirs hands were still moving blood flowing out of peoples body like water 
+that day was horrifying i shouldn't had watched that video back then but what is done cannot be changed now. that incident those visuals 
+still have a deep scare on me and i want to forget it but i know i will not be able to forget it .
+the conclusion that i drew that day from that experience of mine was that never touch a thing that says dangeorous for yourself no matter 
+how tempting it is , and the same concern i have for the kids of this era they shouldnot allowed to have sm*/
+
+#include <iostream>
+using namespace std;
+class Cars{
+      public:
+      int age(int a , int b , int s )
+      {
+            cout<<"enter the age of father:"<<endl;
+            cin>>a;
+            cout<<"enter the age of son:"<<endl;
+            cin>>b;
+            s = a-b;
+            cout<<"the difference between the age of father and son is :"<<s<<endl;
+      };
+
+};
+int main()
+{
+   Cars a;
+   a.age( 54 ,  25 , 21);
+   return 0;
+}
